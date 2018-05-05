4cd7bf3d3f176dc7fa46a82b6837d92aa9373da6
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-05-05 18:23:34 -0400

    Functional app - no movement of patrons

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,26 +1,51 @@
 $(document).ready(function(){
     console.log('linked js')
-
     $.get("/api/patrons")
     .then (function(data){
         console.log(data)
     })
-
 })
-        $("#reserveButton").on("click", function(event) {
-            event.preventDefault();
-      
-            var newPatron = {
-              name: $("#reserve_name").val().trim(),
-              phone: $("#reserve_phone").val().trim(),
-              email: $("#reserve_email").val().trim()
-            };
-      
-            // Question: What does this code do??
-            $.post("/api/patrons", newPatron)
-              .then(function(data) {
-                console.log(data);
-                alert("Adding you to waiting list... We'll text you when we a Mom becomes available.");
-              });
-            console.log(newPatron);
-          });
+
+
+$("#reserveButton").on("click", function(event) {
+    // event.preventDefault();
+
+    var newPatron = {
+        name: $("#reserve_name").val().trim(),
+        phone: $("#reserve_phone").val().trim(),
+        email: $("#reserve_email").val().trim()
+    };
+    console.log(newPatron);
+    
+    // Question: What does this code do??
+    $.post("/api/patrons", newPatron)
+    .then(function(data) {
+        console.log(data);
+        alert("Adding you to waiting list... We'll text you when we a Mom becomes available.");
+    });
+    // $("#reserve_name").val().empty();
+    
+});
+        
+$.get("/api/patrons", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div class='card'>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "patron-well-" + i);
+        
+        if (i<5){
+            $("#tableWell-01").append(wellSection);
+
+            $("#patron-well-" + i).append("<h3>" + data[i].name + "</h3>"+"<hr>");
+            $("#patron-well-" + i).append("<h4>Phone: " + data[i].phone + "</h4>");
+            $("#patron-well-" + i).append("<h4>E-mail: " + data[i].email + "</h4>"); 
+        }
+        else{
+            $("#tableWell-02").append(wellSection);
+
+            $("#patron-well-" + i).append("<h3>" + data[i].name + "</h3>"+"<hr>");
+            $("#patron-well-" + i).append("<h4>Phone: " + data[i].phone + "</h4>");
+            $("#patron-well-" + i).append("<h4>E-mail: " + data[i].email + "</h4>");
+        }       
+    }
+  });
