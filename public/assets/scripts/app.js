225b8faa2af58a580884767e1cd6109f91643afe
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-05-05 14:07:24 -0400

    edited something..

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -6,21 +6,21 @@ $(document).ready(function(){
         console.log(data)
     })
 
-    $("#reserveButton").on("click", function(event) {
-        event.preventDefault();
-  
-        var newPatron = {
-          name: $("#reserve_name").val().trim(),
-          phone: $("#reserve_phone").val().trim(),
-          email: $("#reserve_email").val().trim()
-        };
-  
-        // Question: What does this code do??
-        $.post("/api/patrons", newPatron)
-          .then(function(data) {
-            console.log(data);
-            alert("Adding you to waiting list... We'll text you when we a Mom becomes available.");
-          });
-  
-      });
 })
+        $("#reserveButton").on("click", function(event) {
+            event.preventDefault();
+      
+            var newPatron = {
+              name: $("#reserve_name").val().trim(),
+              phone: $("#reserve_phone").val().trim(),
+              email: $("#reserve_email").val().trim()
+            };
+      
+            // Question: What does this code do??
+            $.post("/api/patrons", newPatron)
+              .then(function(data) {
+                console.log(data);
+                alert("Adding you to waiting list... We'll text you when we a Mom becomes available.");
+              });
+            console.log(newPatron);
+          });
