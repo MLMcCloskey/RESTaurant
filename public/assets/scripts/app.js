a66296b5372d613773c3ff8727d833a60d87d119
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-05-05 13:55:52 -0400

    grabbing data from reserve form

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -5,4 +5,22 @@ $(document).ready(function(){
     .then (function(data){
         console.log(data)
     })
+
+    $("#reserveButton").on("click", function(event) {
+        event.preventDefault();
+  
+        var newPatron = {
+          name: $("#reserve_name").val().trim(),
+          phone: $("#reserve_phone").val().trim(),
+          email: $("#reserve_email").val().trim()
+        };
+  
+        // Question: What does this code do??
+        $.post("/api/patrons", newPatron)
+          .then(function(data) {
+            console.log(data);
+            alert("Adding you to waiting list... We'll text you when we a Mom becomes available.");
+          });
+  
+      });
 })
